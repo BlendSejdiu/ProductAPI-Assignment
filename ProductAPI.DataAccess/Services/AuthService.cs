56b208f07ade11d820f03a0483c4146aca14ae31
Blend Sejdiu <blendi124sejdiu@gmail.com>
2025-11-29 13:17:58 +0100

    Added models to dbcontext and added Auth service with interface

---
+++ ProductAPI.DataAccess/Services/AuthService.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ProductAPI.DataAccess.Services.IServices;
+using ProductAPI.Models.DTO;
+using ProductAPI.Models.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ProductAPI.DataAccess.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _config;
+
+        public AuthService(ApplicationDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        public async Task<TokenResponseDTO?> LoginAsync(UserDTO request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == request.Email);
+            if (user == null)
+                return null;
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+                return null;
+
+            var response = new TokenResponseDTO
+            {
+                AccessToken = CreateToken(user),
+                RefreshToken = await GenerateAndSaveRefreshTokenAsync(user),
+            };
+
+            return response;
+            
+        }
+
+        public async Task<User?> RegisterAsync(UserDTO request)
+        {
+            if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+                return null;
+
+            if (string.IsNullOrWhiteSpace(request.Password))
+                return null;            
+
+            var user = new User();
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+            user.Id = new Guid();
+            user.Username = request.Username;
+            user.Email = request.Email;
+            user.PasswordHash = hashedPassword;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        private async Task<User?> ValidateRefreshToken(Guid userId, string refreshToken)
+        {
+            var user = await _context.Users.FindAsync(userId);
+
+            if (user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.UtcNow)
+                return null;
+
+            return user;
+        }
+
+        private string GenerateRefreshToken()
+        { 
+            var random = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+
+            rng.GetBytes(random);
+
+            return Convert.ToBase64String(random);
+        }
+
+        private async Task<string> GenerateAndSaveRefreshTokenAsync(User user)
+        { 
+            var refreshToken = GenerateRefreshToken();
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+            user.TokenCreated = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+            return refreshToken;    
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetValue<string>("AppSettings:Token")!));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(issuer: _config.GetValue<string>("AppSettings:Issuer"),
+                audience: _config.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<TokenResponseDTO?> RefreshTokensAsync(RefreshTokenRequestDTO request)
+        {
+            var user = await ValidateRefreshToken(request.UserId, request.RefreshToken);
+            if (user is null)
+                return null;
+
+            var response = new TokenResponseDTO
+            {
+                AccessToken = CreateToken(user),
+                RefreshToken = await GenerateAndSaveRefreshTokenAsync(user),
+            };
+
+            return response;
+        }
+    }
+}
---
+++ ProductAPI.DataAccess/Services/IServices/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using ProductAPI.Models.DTO;
+using ProductAPI.Models.Models;
+
+namespace ProductAPI.DataAccess.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<User?> RegisterAsync(UserDTO request);
+        Task<TokenResponseDTO?> LoginAsync(UserDTO request);
+        Task<TokenResponseDTO?> RefreshTokensAsync(RefreshTokenRequestDTO request);
+    }
+}
